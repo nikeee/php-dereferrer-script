7ff67523d0f44aa8151f355f501a794f0410f2e1
nikeee13 <nikeee@outlook.com>
2014-05-18 15:44:25 +0200

    Add config.php

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+	$htmlRedirect = true;
+
+	// supported protocols for auto-prefixing
+	$protocols = [
+		'http://',
+		'https://'
+	];
+	$defaultProtocol = $protocols[0]; // "site.tld" will be prefixed with this one. E.g. "http://site.tld"
+
+
+
+	// Don't know where to place this and I don't want a extra file for this.
+	function striposarray($haystack, array $needles)
+	{
+		for($i = 0; $i < count($needles); ++$i)
+		{
+			$pos = stripos($haystack, $needles[$i]);
+			if($pos > -1)
+				return $pos;
+		}
+		return -1;
+	}
+
