d31b639bfc19938614531eee4335b27d6e552830
nikeee13 <nikeee@outlook.com>
2014-05-18 15:09:02 +0200

    Fix indentation

--- redirect.php
+++ redirect.php
@@ -7,6 +7,7 @@
 	</head>
 	<body>
 	<div align="center">
+		<h1>LOLREDIREECT</h1>
 		<p><?php echo $url; ?> &hellip;</p>
 	</div>
 	</body>
--- splash.php
+++ splash.php
@@ -4,37 +4,37 @@
 		<meta charset="utf-8">
 		<title>Dereferrer Service</title>
 		<style>
-		body {
-			background-color: #fff;
-			padding: 0;
-			margin: 0 auto;
-			width: 900px;
-			text-align: center;
-		}
-		h1, p {
-			text-transform: uppercase;
-			font-family: Verdana, Arial, Helvetica, sans-serif;
-			color: #262B2E;
-			padding: 0;
-			margin: 0;
-		}
-		h1 {
-			font-size: 120px;
-		}
-		p {
-			margin: 100px 0 100px 0;
-		}
-		div.about {
-			font-family: Verdana, Arial, Helvetica, sans-serif;
-			font-size: 12px;
-			color: #ccc;
-			padding: 0;
-			margin: 0;
-		}
-		div.about a {
-			color: #ccc;
-			text-decoration: none;
-		}
+			body {
+				background-color: #fff;
+				padding: 0;
+				margin: 0 auto;
+				width: 900px;
+				text-align: center;
+			}
+			h1, p {
+				text-transform: uppercase;
+				font-family: Verdana, Arial, Helvetica, sans-serif;
+				color: #262B2E;
+				padding: 0;
+				margin: 0;
+			}
+			h1 {
+				font-size: 120px;
+			}
+			p {
+				margin: 100px 0 100px 0;
+			}
+			div.about {
+				font-family: Verdana, Arial, Helvetica, sans-serif;
+				font-size: 12px;
+				color: #ccc;
+				padding: 0;
+				margin: 0;
+			}
+			div.about a {
+				color: #ccc;
+				text-decoration: none;
+			}
 		</style>
 	</head>
 	<body>
